145836448a58daff25475a2ae53d6b4cb4ecdcf9
DanielaBeltranCruz <vi12982@innovaccion.mx>
2022-05-01 13:02:00 -0600

    Refactorizando la lógica que se ejecuta sobre la lista de explorers - Explorer Service

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const Reader = require("./lib/utils/Reader")
+const ExplorerService = require("./lib/services/ExplorerService")
 
 const explorers = Reader.readJsonFile("explorers.json") // esto regresa la lista de explorers del archivo
 
-console.log(explorers)
+//console.log(explorers)
+
+// Aplicación del ExplorerService sobre la lista de explorers
+console.log(ExplorerService.filterByMission(explorers, "node"))
+console.log(ExplorerService.getAmountOfExplorersByMission(explorers, "node"))
+console.log(ExplorerService.getExplorersUsernamesByMission(explorers, "node"))
