3c55979a58b24495224a2d15de980beba79913e5
DanielaBeltranCruz <vi12982@innovaccion.mx>
2022-05-01 13:27:01 -0600

    Refactorizando la lógica - FizzBuzz Service

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Reader = require("./lib/utils/Reader")
 const ExplorerService = require("./lib/services/ExplorerService")
+const FizzbuzzService = require("./lib/services/FizzbuzzService")
 
 const explorers = Reader.readJsonFile("explorers.json") // esto regresa la lista de explorers del archivo
 
@@ -9,3 +10,7 @@ const explorers = Reader.readJsonFile("explorers.json") // esto regresa la lista
 console.log(ExplorerService.filterByMission(explorers, "node"))
 console.log(ExplorerService.getAmountOfExplorersByMission(explorers, "node"))
 console.log(ExplorerService.getExplorersUsernamesByMission(explorers, "node"))
+
+// Validaciones
+const explorer = {score: 15}
+console.log(FizzbuzzService.applyValidationInExplorer(explorer))
